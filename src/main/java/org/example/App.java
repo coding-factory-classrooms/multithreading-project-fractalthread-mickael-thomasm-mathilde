6ccbd509c8a7b15c4f9d66ccd486f9eb3367fcb8
Thomas Guillouët <thomas.guillouet@outlook.com>
2021-01-06 16:58:21 +0100

    Creating a main file in the same package as FractalGenerator
    This file will be used only to write the execution times in the stats.md file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,30 +3,14 @@ package org.example;
 import org.example.core.Conf;
 import org.example.core.Template;
 import org.example.middlewares.LoggerMiddleware;
-import org.example.utils.fractals.FractalGenerator;
 import spark.Spark;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
-        int size = 1000;
-        long start = System.currentTimeMillis();
-        FractalGenerator fractalGenerator = new FractalGenerator(size, size);
-        BufferedImage image = fractalGenerator.generateImage();
-        try {
-            ImageIO.write(image, "jpg", new File("fractals/fractal.jpg"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        long elapsedTime = System.currentTimeMillis() - start;
-        System.out.println("Elapsed: " + elapsedTime + " ms");
-
         initialize();
+
         Spark.get("/", (req, res) -> {
             return Template.render("home.html", new HashMap<>());
         });
