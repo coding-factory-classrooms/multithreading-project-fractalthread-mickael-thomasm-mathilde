5689110cb1bf512ced4435efcb8cd3ba68df200b
Thomas Guillouët <thomas.guillouet@outlook.com>
2021-01-13 12:39:47 +0100

    Updating the mandelbrot algorithm

--- src/main/java/org/example/utils/fractals/fractals/Mandelbrot.java
+++ src/main/java/org/example/utils/fractals/fractals/Mandelbrot.java
@@ -5,42 +5,41 @@ import java.util.List;
 import java.util.concurrent.Callable;
 
 /**
- * Source: https://gist.github.com/dackerman/7bc682d41ce632602af9e1a9858c82fc
+ * Sources: https://gist.github.com/dackerman/7bc682d41ce632602af9e1a9858c82fc, https://lodev.org/cgtutor/juliamandelbrot.html
  */
 public class Mandelbrot implements Fractal, Callable<List<Integer>>{
     public static int MAX_ITERATIONS = 5000;
-    private static final int MOVE_MULTIPLIER = 100;
 
-    private final double xSkip;
-    private final double ySkip;
-    private final double x0;
-    private final double y0;
     private final FractalConf fractalConf;
     private final int y;
 
     public Mandelbrot(int y, FractalConf configuration) {
         this.y = y;
         this.fractalConf = configuration;
-        this.x0 = -1.5;
-        this.xSkip = (0.5 - this.x0) / configuration.width;
-        this.y0 = -1.0;
-        this.ySkip = (1.0 - this.y0) / configuration.height;
     }
 
     @Override
     public int calculatePixel(int pixelX, int pixelY) {
-        double x = (x0 + (pixelX + (fractalConf.moveX * MOVE_MULTIPLIER)) * xSkip) / fractalConf.zoom;
-        double y = (y0 + (pixelY + (fractalConf.moveY * MOVE_MULTIPLIER)) * ySkip) / fractalConf.zoom;
-        double ix = 0;
-        double iy = 0;
-        int iteration = 0;
-        while (ix * ix + iy * iy < 4 && iteration < MAX_ITERATIONS) {
-            double xtemp = ix * ix - iy * iy + x;
-            iy = 2 * ix * iy + y;
-            ix = xtemp;
-            iteration++;
+        double pr, pi;           //real and imaginary part of the pixel p
+        double newRe, newIm, oldRe, oldIm;
+
+        pr = 1.5 * (pixelX - fractalConf.width / 2) / (0.5 * fractalConf.zoom * fractalConf.width) + fractalConf.moveX;
+        pi = (pixelY - fractalConf.height / 2) / (0.5 * fractalConf.zoom * fractalConf.height) + fractalConf.moveY;
+        newRe = newIm = oldRe = oldIm = 0; //these should start at 0,0
+
+        int i;
+        for(i = 0; i < fractalConf.maxIterations; i++)
+        {
+            //remember value of previous iteration
+            oldRe = newRe;
+            oldIm = newIm;
+            //the actual iteration, the real and imaginary part are calculated
+            newRe = oldRe * oldRe - oldIm * oldIm + pr;
+            newIm = 2 * oldRe * oldIm + pi;
+            //if the point is outside the circle with radius 2: stop
+            if((newRe * newRe + newIm * newIm) > 4) break;
         }
-        return iteration;
+        return i;
     }
 
     @Override
